e7a96311dd5e567206fa7e5a2902612735c585e7
dullat <dullat@users.noreply.github.com>
2024-04-14 22:32:31 +0530

    update

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 ScrollReveal({
   reset: true,
-  distance: '50px',
+  distance: "50px",
   duration: 1000,
-  delay: 400
+  delay: 400,
 });
 
-ScrollReveal().reveal('h2', { delay: 300, opacity: 0, distance: 0})
-ScrollReveal().reveal('.image', { delay: 100, origin: 'left'})
-ScrollReveal().reveal('.text-box', { delay: 200, origin: 'right' })
+ScrollReveal().reveal("h1", { delay: 300, opacity: 0, distance: 0 });
+ScrollReveal().reveal(".image", { delay: 100, origin: "left" });
+ScrollReveal().reveal(".text-box", { delay: 200, origin: "right" });
+ScrollReveal().reveal(".social", { delay: 200 });
+ScrollReveal().reveal(".social2", { delay: 400, origin: "left" });
